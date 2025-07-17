b98579591996f711fad01fcced6763d34292b219
noelkun <sfyuu4598@gmail.com>
2025-07-17 18:28:15 +0530

    adding middleware to server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,17 @@
 import express from "express";
+import cors from "cors"
 import { ENV } from "../config/env.js";
 import { connectDB } from "../config/db.js";
+import {clerkMiddleware} from "@clerk/express"
+
+
+const app = express();
 
 //middlewares
-const app = express();
+app.use(cors())
+app.use(express.json())
+app.use(clerkMiddleware())
+
 
 connectDB().then(r => app.listen(ENV.PORT, () => {
   console.log("server is up and running on PORT:", ENV.PORT);
