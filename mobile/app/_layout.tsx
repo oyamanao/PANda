ce412d6e2f5b2652ed6e9ae0aa64e029fb14c9e9
noelkun <sfyuu4598@gmail.com>
2025-08-10 18:57:09 +0530

    auth pages added

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,10 +1,15 @@
 import { Stack } from "expo-router";
 import "../global.css";
+import { ClerkProvider } from '@clerk/clerk-expo'
+import { tokenCache } from '@clerk/clerk-expo/token-cache'
+
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-    </Stack>
+    <ClerkProvider tokenCache={tokenCache}>
+      <Stack>
+        <Stack.Screen name="(auth)" options={{headerShown: false}} />
+      </Stack>
+    </ClerkProvider>
   );
 }
